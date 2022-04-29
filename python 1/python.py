61b11d3649a8fb604ae403ea73280ca01d130ea5
Javohirbek009 <104622975+Javohirbek009@users.noreply.github.com>
2022-04-29 12:10:19 +0300

    Add files via upload

---
+++ python 1/python.py
@@ -0,0 +1,72 @@
+# ruchka = 'qora'
+# print(ruchka)
+
+
+
+# name = "javohir"
+# last_name = " nematov"
+# print(name + last_name)
+
+
+
+# num1 = input("sonni kiriting: ")
+# num2 = input("ikkichi sonni kiriting: ")
+# result = num1 + num2
+# print('Natija: ' + result)
+
+
+
+# data types: string, number; integer.
+
+
+
+# num1 = int(input('sonni kiriting: '))
+# num2 = int(input('ikkichi sonni kiriting: '))
+# result = num1 / num2
+# print('Natija: ' + str(result))
+
+
+
+# num = int(input('sonni kiriting: '))
+# float = 23.4
+# print(num + float)
+
+
+
+        # Shartli operatorlar #
+# num = 2008
+
+# if num == 2008:
+#     print('bu raqamlar teng!')
+# else:
+#     print('bu raqamlar teng emas!')
+
+
+
+
+
+# num1 = int(input('sonni kiriting: '))
+# num2 = int(input('ikkichi sonni kiriting: '))
+# value = input('Amalni kiriting ')
+
+# if value == '+':
+#     print("Natija: " + str(num1 + num2))
+# elif value == '-':
+#     print("Natija: " + str(num1 - num2))
+# elif value == '*':
+#     print("Natija: " + str(num1 * num2))
+# elif value == '/':
+#     print("Natija: " + str(num1 / num2))
+
+
+
+value = input('me: ')
+
+if value == 'salom':
+    print("PC: " + 'alik')
+
+elif value == 'ahvollar qanday':
+    print("PC: " + 'yaxshi ozingizdachi?')
+
+elif value == 'yaxshi':
+    print("PC: " + 'mayli hayr salomat bo`ling!')
